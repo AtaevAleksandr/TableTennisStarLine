86754c9f66ad468370628b95564d4c11dbb10e22
Aleksandr Ataev <onlyaa94@mail.ru>
2023-06-02 22:49:34 +0300

    Initial Commit

---
+++ TT StarLine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TT StarLine
+//
+//  Created by Aleksandr Ataev on 02.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
